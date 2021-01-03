b6bc59b88875dae0b3e2102797053b058c569391
James Astell <james.a.astell@gmail.com>
2021-01-03 18:10:20 +0000

    Add intents to all_training_dialog
    Speech now has intents show before to allow for training

--- processing_data.py
+++ processing_data.py
@@ -21,7 +21,7 @@ def add_dialogue_index(prefix, all_dialogue):  # TO DO
 
 
 train_dir = "data/dstc8-schema-guided-dialogue/train"
-all_dialogs = []  # Create an empty list array outside of loop
+all_dialogs = []  # Create an empty list array
 
 for file_name in tqdm.tqdm(os.listdir(train_dir), desc='Processing'):  # Display progress bar during loop
 
@@ -30,21 +30,26 @@ for file_name in tqdm.tqdm(os.listdir(train_dir), desc='Processing'):  # Display
 
     file_path = os.path.join(train_dir, file_name)  # Create new file path for each run of loop
 
+    # Open each dialog file
     with open(file_path, "r") as f:
-        data = json.load(f)  # Open each dialog file
+        data = json.load(f)
 
     part_dialogs = []  # Create an empty list for each dialog to be added too
 
     for dialog in data:
-        one_dialog = [[item['speaker'].capitalize(), item['utterance']] for item in dialog['turns']]  # TO DO
-        part_dialogs.append(one_dialog)  # Add the separated dialog to the end of list
+        for item in dialog['turns']:
+            utterance = [[item['speaker'].capitalize(), item['utterance']]]  # Extract the system and user speech
+            for item in item['frames']:
+                intent = [[item['act']] for item in item['actions']]  # Extract the intent of the following speech
+                part_dialogs.append(intent)
+                part_dialogs.append(utterance)
 
     all_dialogs.extend(part_dialogs)  # Add all elements of new dialog to overall list
 
-all_dialogs = add_dialogue_index("schema_dialog", all_dialogs)  # Call the add_dialogue_index function
-
 file_path = os.path.join(current_dir, "data/all_training_dialog.json")  # Defines the new list .json file
 logger.info(f"Saving Schema Dialog data to {file_path}")
+
+# Save the .json file
 with open(file_path, "w") as fp:
-    json.dump(all_dialogs, fp, indent=4)  # Saving the .json file
+    json.dump(all_dialogs, fp, indent=4)
 logger.info("Processing Complete!")
